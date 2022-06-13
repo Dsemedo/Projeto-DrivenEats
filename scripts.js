696968b7177dc2a78c07b283fdf8965dbfd1668f
Dsemedo <dsemedoo@gmail.com>
2022-06-13 01:38:10 -0300

    Html ok, CSS ok, JS 60%

--- scripts.js
+++ scripts.js
@@ -1,10 +1,35 @@
 
 function selecionarPrato(elemento){
-    const selecionado = document.querySelector(".opcao");
+    const pratoClicado = document.querySelector(".borda-verde");
 
-    if (selecionado !== null){
-        selecionado.classList.remove("borda-verde");
+    if (pratoClicado !== null){
+        pratoClicado.classList.remove("borda-verde")
     }
 
-    elemento.classlist.add("borda-verde");    
+    elemento.classList.add("borda-verde");
+    
 }
+
+function selecionarBebida(elemento){
+    const bebidaClicada = document.querySelector(".borda-verde");
+
+    if (bebidaClicada !== null){
+        bebidaClicada.classList.remove("borda-verde")
+    }
+
+    elemento.classList.add("borda-verde");
+    
+}
+
+function selecionarSobremesa(elemento){
+    const sobremesaClicada = document.querySelector(".borda-verde");
+
+    if (sobremesaClicada !== null){
+        sobremesaClicada.classList.remove("borda-verde")
+    }
+
+    elemento.classList.add("borda-verde");
+    
+}
+
+function ativarBotao(elemento)
